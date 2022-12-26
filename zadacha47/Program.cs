58e4066f9c9cb831d81d21bfeb3f997bde841305
Fedor <fedor.kochkin48@gmail.com>
2022-12-27 00:59:16 +0300

    решение задачи 47

---
+++ zadacha47/Program.cs
@@ -0,0 +1,18 @@
+﻿void InputMatrix(double[,] matrix)
+{
+    Random rnd = new Random(); 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * 50 - 10, 2);; // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
